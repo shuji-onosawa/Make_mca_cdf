0b885f9b3c3093a77add5fcadc3ae664734fd415
{shuji <{shuji.onosawa.p8@dc.tohoku.ac.jp}>
2023-02-09 19:00:09 +0900

    change csv files

--- onosawa/src/compare_cdf_files.py
+++ onosawa/src/compare_cdf_files.py
@@ -1,24 +1,28 @@
 import cdflib
 import numpy as np
-import sys
+
+
+def compare_cdf_files(input_cdf_name, compared_cdf_name):
+
+    created_xary = cdflib.cdf_to_xarray(input_cdf_name)
+    existing_xary = cdflib.cdf_to_xarray(compared_cdf_name)
+
+    # compair these xarray
+    flag = np.allclose(created_xary.to_array(), existing_xary.to_array())
+    return flag
+
+
 with open("../execute/compare_8scdf_files.txt", "w") as f:
-    sys.stdout = f
     for date in ['0201', '0202', '0203', '0204', '0205', '0206', '0207', '0208', '0209', '0210',
                 '0211', '0212', '0213', '0214', '0215', '0216', '0217', '0218', '0219', '0220',
                 '0221', '0222', '0223', '0224', '0225', '0226', '0227', '0228']:
-        
-        createdcdf_name = '../CDF-H1_test/1990/ak_h1_mca_1990' + date + '_v02.cdf'
-        existingcdf_name = '/Document/Akebono_analysis/VLF_mca/Akebono_MCA_data/ak_h1_mca_1990' + date + '_v02.cdf'
-        
-        createdcdf_name = existingcdf_name
-        # convert cdf to xarray
-        try:
-            created_xary = cdflib.cdf_to_xarray(createdcdf_name)
-            existing_xary = cdflib.cdf_to_xarray(existingcdf_name)
-        except:
-            print('error')
-            continue
 
-        # compair these xarray
-        flag = np.allclose(created_xary.to_array(), existing_xary.to_array())
-        print(flag)
+        createdcdf_name = '../data/CDF-H1_test/1990/ak_h1_mca_1990' + date + '_v02.cdf'
+        existingcdf_name = '../data/CDF-H1_provided_by_Kasahara/ak_h1_mca_1990' + date + '_v02.cdf'
+        print(date)
+        f.write(date+'\n')
+        try:
+            flag = compare_cdf_files(createdcdf_name, existingcdf_name)
+            f.write('same file:'+str(flag)+'\n')
+        except Exception as e:
+            f.write(str(e)+'\n')
